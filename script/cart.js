e31834399154c19f17fc4919eeb9a5c14d59196d
Ercel Williams <you@example.com>
2022-10-14 11:27:46 -0400

    Search Bar

---
+++ script/cart.js
@@ -0,0 +1,10 @@
+const products = [
+    {
+        id: 0,
+        name:"Yeezy Foam Runner",
+        price: 80.00,
+        instock: 10,
+        description: "Yeezy Foam Runner 'Mineral Blue'"
+
+    }
+]
